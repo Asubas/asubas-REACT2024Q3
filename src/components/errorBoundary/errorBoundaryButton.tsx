4683178f9d5be6176163c61ca4493491597809f1
Леха <znaxor74@mail.ru>
2024-07-08 19:22:44 +0300

    refactor: change ErrorBoundaryButton  from class component to function component

--- src/components/errorBoundary/errorBoundaryButton.tsx
+++ src/components/errorBoundary/errorBoundaryButton.tsx
@@ -1,26 +1,20 @@
 import './errorBoundary.scss';
-import { PureComponent, ReactNode } from 'react';
-import { ErrorBoundaryButtonState } from '../../interfaces/errorBoundaryInterfaces';
 import dogSvg from '../../assets/svg-dog-icon.svg';
-class ErrorBoundaryButton extends PureComponent<unknown, ErrorBoundaryButtonState> {
-  state: ErrorBoundaryButtonState = {
-    isActive: false,
+import { useState } from 'react';
+function ErrorBoundaryButton() {
+  const [errorState, setErrorState] = useState(false);
+
+  const handleClick = () => {
+    setErrorState(true);
   };
 
-  handleClick = () => {
-    this.setState({ isActive: true });
-  };
-
-  render(): ReactNode {
-    if (this.state.isActive)
-      throw new Error('Error when trying to load a DOM tree or its children');
-    return (
-      <button className="check-error-boundary_button" type="button" onClick={this.handleClick}>
-        Check Error Boundary
-        <img className="check-error-boundary_svg" src={dogSvg} alt="dog svg labrador" />
-      </button>
-    );
-  }
+  if (errorState) throw new Error('Error when trying to load a DOM tree or its children');
+  return (
+    <button className="check-error-boundary_button" type="button" onClick={handleClick}>
+      Check Error Boundary
+      <img className="check-error-boundary_svg" src={dogSvg} alt="dog svg labrador" />
+    </button>
+  );
 }
 
 export { ErrorBoundaryButton };
