b3df41d9ff3edd11ea81149c9ab47a685187ab83
Леха <znaxor74@mail.ru>
2024-08-15 00:31:39 +0300

    fix: some code sharing and styled for disable button

--- src/components/formComponent/regularFormComponent.tsx
+++ src/components/formComponent/regularFormComponent.tsx
@@ -1,19 +1,17 @@
 import { useDispatch, useSelector } from 'react-redux';
 import './FormComponent.scss';
-import { Form, useNavigate } from 'react-router-dom';
+import { Form } from 'react-router-dom';
 import { RootState } from '../../app/store';
 import { setCountries } from '../../app/slices/CountrySlices';
 import { useEffect, useState } from 'react';
-import * as Yup from 'yup';
-import { formReducer } from '../../app/slices/FormSlices';
+import { useFormSubmit } from './submitForm';
 
 function RegularFormComponent() {
   const [errors, setErrors] = useState<{ [key: string]: string | undefined }>({});
   const [file, setFile] = useState<File | null>(null);
   const dispatch = useDispatch();
-  const navigate = useNavigate();
   const countries = useSelector((state: RootState) => state.country.countries);
-
+  console.log(errors);
   useEffect(() => {
     const fetchCountries = () => {
       const allCountries = ['Russia', 'USA', 'Canada', 'UK', 'Germany', 'France'];
@@ -23,89 +21,7 @@ function RegularFormComponent() {
     fetchCountries();
   }, [dispatch]);
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-    const formSchema = Yup.object({
-      name: Yup.string()
-        .trim()
-        .matches(/^[A-Z]/, 'The first letter must be capitalized!')
-        .required('This field is required!'),
-      age: Yup.number()
-        .integer()
-        .positive('The number must be positive')
-        .required('This field is required!'),
-      email: Yup.string().email('Please enter correct email').required('This field is required!'),
-      password: Yup.string()
-        .matches(/[0-9]/, 'Password must contain at least 1 digit')
-        .matches(/[a-z]/, 'The password must contain at least one lowercase letter.')
-        .matches(/[A-Z]/, 'The password must contain at least one capital letter.')
-        .matches(/[\W_]/, 'The password must contain at least one special character.')
-        .required('This field is required!'),
-      confirmPassword: Yup.string()
-        .oneOf([Yup.ref('password')], 'Passwords dont match')
-        .required('This field is required!'),
-      file: Yup.mixed()
-        .test('fileSize', 'File size must not exceed 2MB.', function (value) {
-          if (!value) return false;
-          const file = value as File;
-          return file.size <= 2 * 1024 * 1024;
-        })
-        .test('fileType', 'Only JPG and PNG are supported.', function (value) {
-          if (!value) return false;
-          const file = value as File;
-          return file.type === 'image/jpeg' || file.type === 'image/png';
-        }),
-      TC: Yup.bool().oneOf([true], 'You need open chest!').required('Required'),
-    });
-
-    try {
-      const TC = formData.get('TC') === 'on';
-      const ageValue = e.currentTarget.age.value;
-      await formSchema.validate(
-        {
-          name: formData.get('name'),
-          age: ageValue === '' ? undefined : Number(ageValue),
-          email: formData.get('email'),
-          password: formData.get('password'),
-          confirmPassword: formData.get('confirmPassword'),
-          file: formData.get('fileLoader'),
-          TC: TC,
-        },
-        { abortEarly: false },
-      );
-      if (file) {
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onloadend = () => {
-          const base64data = reader.result;
-          const payload = {
-            name: formData.get('name'),
-            age: formData.get('age'),
-            email: formData.get('email'),
-            password1: formData.get('password'),
-            password2: formData.get('confirmPassword'),
-            gender: formData.get('sex'),
-            profilePicture: base64data,
-            country: formData.get('country'),
-          };
-          dispatch(formReducer(payload));
-        };
-      }
-      setErrors({});
-      navigate('/');
-    } catch (error: unknown) {
-      if (error instanceof Yup.ValidationError) {
-        const errors = error.inner.reduce((acc: { [key: string]: string }, err) => {
-          if (err.path) {
-            acc[err.path] = err.message;
-          }
-          return acc;
-        }, {});
-        setErrors(errors);
-      }
-    }
-  };
+  const handleSubmit = useFormSubmit(dispatch, setErrors, file);
 
   const [type, setType] = useState('password');
   const [typeConfirm, setTypeConfirm] = useState('password');
@@ -212,7 +128,11 @@ function RegularFormComponent() {
           <option key={index} value={country} />
         ))}
       </datalist>
-      <button className="submitButton" type="submit">
+      <button
+        className="submitButton"
+        type="submit"
+        disabled={Object.keys(errors).length === 0 ? false : true}
+      >
         I want to become a pirate!
       </button>
     </Form>
---
+++ src/components/formComponent/submitForm.tsx
@@ -0,0 +1,101 @@
+import * as Yup from 'yup';
+import { formReducer } from '../../app/slices/FormSlices';
+import { Dispatch, SetStateAction } from 'react';
+import { AppDispatch } from '../../app/store';
+import { useNavigate } from 'react-router-dom';
+
+export const useFormSubmit = (
+  dispatch: AppDispatch,
+  setErrors: Dispatch<SetStateAction<{ [key: string]: string | undefined }>>,
+  file: File | null,
+) => {
+  const navigate = useNavigate();
+  const formSchema = Yup.object({
+    name: Yup.string()
+      .trim()
+      .matches(/^[A-Z]/, 'The first letter must be capitalized!')
+      .required('This field is required!'),
+    age: Yup.number()
+      .integer()
+      .positive('The number must be positive')
+      .required('This field is required!'),
+    email: Yup.string().email('Please enter correct email').required('This field is required!'),
+    password: Yup.string()
+      .matches(/[0-9]/, 'Password must contain at least 1 digit')
+      .matches(/[a-z]/, 'The password must contain at least one lowercase letter.')
+      .matches(/[A-Z]/, 'The password must contain at least one capital letter.')
+      .matches(/[\W_]/, 'The password must contain at least one special character.')
+      .required('This field is required!'),
+    confirmPassword: Yup.string()
+      .oneOf([Yup.ref('password')], 'Passwords dont match')
+      .required('This field is required!'),
+    file: Yup.mixed()
+      .test('fileSize', 'File size must not exceed 2MB.', function (value) {
+        if (!value) return false;
+        const file = value as File;
+        return file.size <= 2 * 1024 * 1024;
+      })
+      .test('fileType', 'Only JPG and PNG are supported.', function (value) {
+        if (!value) return false;
+        if (file) return file.type === 'image/jpeg' || file.type === 'image/png';
+      }),
+    TC: Yup.bool().oneOf([true], 'You need open chest!').required('Required'),
+  });
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const TC = formData.get('TC') === 'on';
+    const ageValue = e.currentTarget.age.value;
+
+    try {
+      await formSchema.validate(
+        {
+          name: formData.get('name'),
+          age: ageValue === '' ? undefined : Number(ageValue),
+          email: formData.get('email'),
+          password: formData.get('password'),
+          confirmPassword: formData.get('confirmPassword'),
+          file: formData.get('fileLoader'),
+          TC: TC,
+        },
+        { abortEarly: false },
+      );
+
+      const file = formData.get('fileLoader') as File;
+      if (file) {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onloadend = () => {
+          const base64data = reader.result;
+          const payload = {
+            name: formData.get('name'),
+            age: formData.get('age'),
+            email: formData.get('email'),
+            password1: formData.get('password'),
+            password2: formData.get('confirmPassword'),
+            gender: formData.get('sex'),
+            profilePicture: base64data,
+            country: formData.get('country'),
+          };
+          dispatch(formReducer(payload));
+        };
+      }
+
+      setErrors({});
+      navigate('/');
+    } catch (error: unknown) {
+      if (error instanceof Yup.ValidationError) {
+        const errors = error.inner.reduce((acc: { [key: string]: string }, err) => {
+          if (err.path) {
+            acc[err.path] = err.message;
+          }
+          return acc;
+        }, {});
+        setErrors(errors);
+      }
+    }
+  };
+
+  return handleSubmit;
+};
